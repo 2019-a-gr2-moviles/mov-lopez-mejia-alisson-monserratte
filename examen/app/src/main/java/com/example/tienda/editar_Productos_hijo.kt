19a82bfdd06b015ab4f276039d3e5415302baa94
alimonse <alimonse29@gmail.com>
2019-06-02 22:56:38 -0500

    pantallitas

---
+++ examen/app/src/main/java/com/example/tienda/editar_Productos_hijo.kt
@@ -0,0 +1,12 @@
+package com.example.tienda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class editar_Productos_hijo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar__productos_hijo)
+    }
+}
---
+++ examen/app/src/main/java/com/example/tienda/listar_productos.kt
@@ -0,0 +1,12 @@
+package com.example.tienda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class listar_productos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_productos)
+    }
+}
